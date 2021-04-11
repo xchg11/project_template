54be04bbef5663144e24c42747027a270841aab9
xchg11 <58431825+xchg11@users.noreply.github.com>
2021-04-11 16:55:56 +0200

    Update pipeline.groovy

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -1,27 +1,5 @@
 #!groovy
 // commit
-import javax.mail.*
-import javax.mail.internet.*
-
-MAILER_HOST = "rmd5.ru"  // "smtp-relay.gmail.com"
-RECIPIENT_EMAIL = "xchg11@rmd5.ru"
-date_time = new Date().format("yyyy-MM-dd hh:mm")
-props = new Properties()
-
-private void sendMail() {
-    props.put("mail.host", MAILER_HOST);
-    Session session = Session.getDefaultInstance(props)
-    session.setDebug(true);
-
-    MimeMessage message = new MimeMessage(session)
-    message.setFrom("no.reply@rmd5.ru")
-    message.setRecipient(Message.RecipientType.TO, new InternetAddress(RECIPIENT_EMAIL))
-    message.setSubject("A Test email ${date_time}")
-    message.setText("tr!!")
-
-    Transport.send(message)
-}
-
 properties([disableConcurrentBuilds()])
 pipeline {
     agent { 
