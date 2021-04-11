2356e7689a48e3f993a4f536be20291c9bb6d04a
xchg11 <58431825+xchg11@users.noreply.github.com>
2021-04-11 15:44:47 +0200

    Update pipeline.groovy

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -1,1 +1,25 @@
+
 #!groovy
+#ts 
+properties([disableConcurrentBuilds()])
+pipeline {
+    agent { 
+        label 'master'
+        }
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '10'))
+        timestamps()
+    }
+    stages {
+        stage("First step") {
+            steps {
+                sh 'id -u'
+            }
+        }
+        stage("Second step") {
+            steps {
+                sh 'hostname'
+            }
+        }
+    }
+}
