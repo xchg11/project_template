d0e34a7b2c4c078485734c040aa893d0472375db
xchg <xchg@fire7.ru>
2021-04-11 21:17:15 +0700

    Third step

---
+++ jenkins/pipeline.groovy
@@ -0,0 +1,29 @@
+#!groovy
+// commit  
+properties([disableConcurrentBuilds()])
+pipeline {
+    agent { 
+        label 'master'
+        }
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '15', artifactNumToKeepStr: '15'))
+        timestamps()
+    }
+    stages {
+        stage("First step") {
+            steps {
+                sh 'id -u'
+            }
+        }
+        stage("Second step") {
+            steps {
+                sh 'hostname'
+            }
+        }
+        stage(Third step") {
+            steps {
+                sh 'uname -a'
+            }
+        }
+    }
+}
